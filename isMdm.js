1a9824fbeaab424f9712b2b5bcd7408e6d1e4730
Tierney Cyren <hello@bnb.im>
2025-12-13 12:00:00 +0000

    fix: run biome format --write
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- isMdm.js
+++ isMdm.js
@@ -1,37 +1,44 @@
-const { spawnSync } = require('node:child_process')
+const { spawnSync } = require("node:child_process");
 
-function isMdmMacOS () {
-	let enrolled = true // let's assume we're managed and correct ourselves if we prove we're not
+function isMdmMacOS() {
+	let enrolled = true; // let's assume we're managed and correct ourselves if we prove we're not
 
-	const command = spawnSync('/usr/bin/profiles', ['status', '-type', 'enrollment']).stdout.toString()
+	const command = spawnSync("/usr/bin/profiles", [
+		"status",
+		"-type",
+		"enrollment",
+	]).stdout.toString();
 
-	if(command.includes('Enrolled via DEP: No') && command.includes('MDM enrollment: No')) {
-		enrolled = false
+	if (
+		command.includes("Enrolled via DEP: No") &&
+		command.includes("MDM enrollment: No")
+	) {
+		enrolled = false;
 	}
 
-	return enrolled
+	return enrolled;
 }
 
-function isMdmWindows () {
-	let enrolled = true // let's assume we're managed and correct ourselves if we prove we're not
+function isMdmWindows() {
+	let enrolled = true; // let's assume we're managed and correct ourselves if we prove we're not
 
-	const command = spawnSync('dsregcmd', ['/status']).stdout.toString()
+	const command = spawnSync("dsregcmd", ["/status"]).stdout.toString();
 
-	if(!command.includes('MdmUrl')) {
-		enrolled = false
+	if (!command.includes("MdmUrl")) {
+		enrolled = false;
 	}
 
-	return enrolled
+	return enrolled;
 }
 
-function isMdm () {
-	if (process.platform === 'darwin') {
-		return isMdmMacOS()
+function isMdm() {
+	if (process.platform === "darwin") {
+		return isMdmMacOS();
 	}
 
-	if (process.platform === 'win32') {
-		return isMdmWindows()
+	if (process.platform === "win32") {
+		return isMdmWindows();
 	}
 }
 
-module.exports = isMdm
+module.exports = isMdm;
